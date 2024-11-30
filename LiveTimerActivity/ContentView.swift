3920f9dca14746a31afdd0bde2720c508d70d31d
Navin Kumar <navin@eam360.com>
2024-11-30 23:04:42 +0530

    Initial Commit

---
+++ LiveTimerActivity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveTimerActivity
+//
+//  Created by Navin Kumar on 30/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveTimerActivity/LiveTimerActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTimerActivityApp.swift
+//  LiveTimerActivity
+//
+//  Created by Navin Kumar on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveTimerActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
